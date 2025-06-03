05f658f61479b0fed8d56714b1212d9cdefff0dc
Eric O'Callaghan <eric@ericoc.com>
2025-06-02 22:02:32 -0400

    add flask-caching

--- __init__.py
+++ __init__.py
@@ -2,12 +2,14 @@ import logging
 from flask import (
     Flask, flash, render_template, make_response, request, send_from_directory
 )
+from flask_caching import Cache
 from datetime import date, datetime
 from doi import DOI
 
 # Flask.
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
+cache = Cache(app)
 
 # Logging.
 logger = logging.getLogger(__name__)
@@ -27,6 +29,7 @@ logging.basicConfig(
 # Main (Jinja2) template to search DOI.
 @app.route("/")
 def index():
+    _cache = False
     code = 200
     _doi = None
     err_msg = None
@@ -35,8 +38,14 @@ def index():
     # Gather information about submitted DOI ("?doi=" GET parameter).
     if get_doi:
         try:
-            _doi = DOI(get_doi)
-            logger.debug(vars(_doi))
+
+            # Check Flask cache for the DOI information
+            if cache.get(get_doi):
+                _doi = get_doi
+                _cache = True
+            else:
+                _doi = DOI(get_doi)
+                cache.set(get_doi.replace('/', '_'), _doi)
 
         # Handle any errors that may occur.
         except ValueError:
@@ -54,7 +63,12 @@ def index():
         if err_msg:
             flash(err_msg)
 
-    return make_response(render_template("index.html.j2", doi=_doi), code)
+
+    resp = make_response(
+        render_template("index.html.j2", doi=_doi), code
+    )
+    resp.headers["X-DOI-Cache"] = _cache
+    return resp
 
 
 @app.route('/static/<path:path>')
--- config.example.py
+++ config.example.py
@@ -1,2 +1,6 @@
+from pathlib import Path
 DEBUG = False
 SECRET_KEY = "secret123"
+CACHE_TYPE = "FileSystemCache"
+CACHE_DEFAULT_TIMEOUT = 30
+CACHE_DIR = Path(Path.home(), "cache")
