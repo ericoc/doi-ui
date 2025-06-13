d41aa5db74677b3bb903c569aeacb49393b895ae
Eric O'Callaghan <ericoc@seas.upenn.edu>
2025-06-13 13:17:16 -0400

    change example default cache time

--- config.example.py
+++ config.example.py
@@ -2,6 +2,6 @@ from pathlib import Path
 DEBUG = False
 SECRET_KEY = "secret123"
 CACHE_TYPE = "FileSystemCache"
-CACHE_DEFAULT_TIMEOUT = 3600
+CACHE_DEFAULT_TIMEOUT = 300
 CACHE_DIR = Path(Path.home(), "cache")
 CACHE_THRESHOLD = 50
