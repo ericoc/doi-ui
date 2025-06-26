2def39bdef691672e2773acdb2e83f8fe0e751d6
Eric O'Callaghan <eric@ericoc.com>
2025-06-25 21:50:57 -0400

    humanize dates/times

--- doi/templatetags.py
+++ doi/templatetags.py
@@ -1,6 +1,33 @@
+from datetime import date, datetime
+from zoneinfo import ZoneInfo
+
+from django.conf import settings
 from django.template.defaulttags import register
+from django.utils.timesince import timesince, timeuntil
 
 
 @register.filter(name="get_dict_item")
 def get_dict_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter(name="human_time")
+def human_time(when: (date, datetime)) -> str:
+
+    current = None
+    ret = str(when)
+    fmt = "%A, %B %d, %Y"
+
+    if isinstance(when, date):
+        current = date.today()
+
+    if isinstance(when, datetime):
+        current = datetime.now().astimezone(tz=ZoneInfo(settings.TIME_ZONE))
+
+    if current:
+        ret = ""
+        if when <= current:
+            ret += f"{timesince(when)} ago"
+        else:
+            ret += f"in {timeuntil(when)}"
+
+    return ret
