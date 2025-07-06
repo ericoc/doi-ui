1192f6ef381d10901b49f9a96785d167338d8784
Eric O'Callaghan <eric@ericoc.com>
2025-07-06 16:15:02 -0400

    simplify templateview, different colors for 0

--- doi/views.py
+++ doi/views.py
@@ -1,5 +1,4 @@
-from django.core.exceptions import BadRequest
-from django.http.response import Http404
+from django.core.exceptions import BadRequest, ObjectDoesNotExist
 from django.views.generic.base import TemplateView
 
 from .models import DOI
@@ -8,28 +7,23 @@ from .models import DOI
 class HomeView(TemplateView):
     """Main page searches DOI."""
 
-    doi = None
+    doi = ""
     http_method_names = ("get",)
     template_name = "home.html"
 
     def setup(self, request, *args, **kwargs):
-        return super().setup(request, *args, **kwargs)
-
-
-    def dispatch(self, request, *args, **kwargs):
-
         # Create object using DOI submitted via URL.
         submitted_doi = request.GET.get("doi")
         if submitted_doi:
             try:
                 self.doi = DOI(submitted_doi=submitted_doi)
-            except FileNotFoundError as not_found_err:
-                raise Http404(not_found_err)
             except ValueError as val_err:
                 raise BadRequest(val_err)
+            except FileNotFoundError as not_found_err:
+                raise ObjectDoesNotExist(not_found_err)
             except Exception as doi_exc:
                 raise RuntimeError("Unknown error.") from doi_exc
-        return super().dispatch(request, *args, **kwargs)
+        return super().setup(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
