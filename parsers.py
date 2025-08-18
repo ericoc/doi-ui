73a7a2a71671253bf2523cece998f9649adc0b06
Eric O'Callaghan <eric@ericoc.com>
2025-08-18 13:36:27 -0400

    rewrite date parsing, and include timestamp

--- parsers.py
+++ parsers.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime
+from datetime import date, datetime, timezone
 from logging import getLogger
 
 
@@ -8,11 +8,19 @@ logger = getLogger(__name__)
 
 def parse_date(item):
     """
-    Parse "date-time" and "date-parts" chunks from DOI JSON.
+    Parse "timestamp", "date-time", or "date-parts" DOI date/time information.
     """
     if item:
 
+        # UNIX/epoch timestamp (in milliseconds).
+        #   Example: { "timestamp": 1753880563838 }
+        timestamp = item.get("timestamp")
+        if timestamp:
+            timestamp = timestamp / 1000
+            return datetime.fromtimestamp(timestamp, tz=timezone.utc)
+
         # ISO date.
+        #   Example: { "date-time": "2025-07-30T13:02:43Z" }
         iso_dt = item.get("date-time")
         if iso_dt:
             try:
@@ -21,18 +29,29 @@ def parse_date(item):
                 logger.error(f"Failed parsing ISO date: {item}")
                 logger.exception(iso_dt_val_err)
 
-        # Three-part (year, month, day) dictionary of "date-parts".
-        date_parts = item.get("date-parts")
-        if date_parts:
-            try:
-                date_parts = date_parts[0]
+        # Three-part nested list (year, month, day) in "date-parts" dictionary.
+        #   Example: { "date-parts": [ [2025, 7, 1] ] }
+        date_parts_out = item.get("date-parts")
+        if date_parts_out:
+            date_parts = date_parts_out[0]
+
+            # "date-parts" dictionary always has year, but not month/day.
+            if date_parts:
                 year = date_parts[0]
-                month = date_parts[1] or 1
-                day = date_parts[2] or 1
+
+                # Month.
+                try:
+                    month = date_parts[1]
+                except IndexError:
+                    month = 1
+
+                # Day.
+                try:
+                    day = date_parts[2]
+                except IndexError:
+                    day = 1
+
                 return date(year, month, day)
-            except IndexError as date_parts_idx_err:
-                logger.error(f"Failed to parse date parts: {item}")
-                logger.exception(date_parts_idx_err)
 
         return item
 
