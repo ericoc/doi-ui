89d5e8705e1910da79c82afcd5ca1d1b52dd75b5
Eric O'Callaghan <eric@ericoc.com>
2025-08-18 12:35:10 -0400

    parse dates differently, with logging

--- parsers.py
+++ parsers.py
@@ -1,28 +1,39 @@
 from datetime import date, datetime
+from logging import getLogger
 
 
-def parse_date(item: (dict, None)) -> (date, datetime, None):
+# Logging.
+logger = getLogger(__name__)
+
+
+def parse_date(item):
     """
     Parse "date-time" and "date-parts" chunks from DOI JSON.
     """
     if item:
+
+        # ISO date.
         iso_dt = item.get("date-time")
         if iso_dt:
             try:
                 return datetime.fromisoformat(iso_dt)
-            except ValueError:
-                pass
+            except ValueError as iso_dt_val_err:
+                logger.error(f"Failed parsing ISO date: {item}")
+                logger.exception(iso_dt_val_err)
 
+        # Three-part (year, month, day) dictionary of "date-parts".
         date_parts = item.get("date-parts")
         if date_parts:
-            p_date = date_parts[0]
-            if p_date:
-                if len(p_date) == 3:
-                    return date(p_date[0], p_date[1], p_date[2])
-                elif len(p_date) == 2:
-                    return date(p_date[0], p_date[1], 1)
-                elif len(p_date) == 1 and p_date[0] is not None:
-                    return date(p_date[0], 1, 1)
+            try:
+                date_parts = date_parts[0]
+                year = date_parts[0]
+                month = date_parts[1] or 1
+                day = date_parts[2] or 1
+                return date(year, month, day)
+            except IndexError as date_parts_idx_err:
+                logger.error(f"Failed to parse date parts: {item}")
+                logger.exception(date_parts_idx_err)
+
         return item
 
     return None
